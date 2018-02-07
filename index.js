a7a378666db242d993afa98e16ccc6e8aa4449fe
Claire Watson <watson.e.claire@gmail.com>
2018-02-07 14:42:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,17 +23,17 @@ function viewCart() {
     console.log(`Your shopping cart is empty.`)
   }
   if(cart.length === 1){
-    console.log(`In your cart, you have ${Object.keys(cart[0])} at $${Object.values(cart[0])}).`)
+    console.log(`In your cart, you have ${Object.keys(cart[0])} at $${Object.values(cart[0])}.`)
   }
   if(cart.length === 2){
-    console.log(`In your cart, you have ${Object.keys(cart[0])} at $${Object.values(cart[0])}) and ${Object.keys(cart[1])} at $${Object.values(cart[1])}) .`)
+    console.log(`In your cart, you have ${Object.keys(cart[0])} at $${Object.values(cart[0])}) and ${Object.keys(cart[1]} at $${Object.values(cart[1])}) .`)
   }
   if(cart.length >= 3){
     var allContentsInCart = ""
     for(var i=0; i<cart.length-1; i++){
       allContentsInCart += ` ${Object.keys(cart[i])} at $${Object.values(cart[i])}`
     }
-    console.log(`In your cart, you have ${allContentsInCart} and ${Object.keys(cart[-1])} at $${Object.values(cart[-1])}`)
+    console.log(`In your cart, you have ${allContentsInCart} and ${Object.keys(cart[cart.length-1]} at $${Object.values(cart[cart.length-1]}`)
   }
 }
 

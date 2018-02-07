1c07231fdaa16a820db4721b226d9937285ee226
Claire Watson <watson.e.claire@gmail.com>
2018-02-07 15:19:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -66,6 +66,6 @@ function placeOrder(cardNumber) {
   }
   else if(cardNumber){
     console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
-    cart.splice(0,cart.length-1)
+    cart.splice(0,cart.length)
   }
 }

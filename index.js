043aa8cbc63bb4b6608d3ac17f7075b4cb087f82
Claire Watson <watson.e.claire@gmail.com>
2018-02-07 15:19:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,5 +61,11 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if(!cardNumber){
+    console.log("Sorry, we don't have a credit card on file for you.")
+  }
+  else if(cardNumber){
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+    cart.splice(0,cart.length-1)
+  }
 }
